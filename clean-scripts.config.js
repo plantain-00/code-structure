e7ca1d6578abdbbccfbf0387af00efd5086451e0
york yao <yaoao12306@outlook.com>
2017-09-17 14:43:34 +0800

    update dependencies

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -6,7 +6,25 @@ const execAsync = util.promisify(childProcess.exec)
 module.exports = {
   build: [
     'rimraf dist/',
-    'tsc -p src/'
+    'tsc -p src/',
+    `node dist/index.js src/*.ts -o demo/result.html --exclude src/*.d.ts`,
+    async () => {
+      const { createServer } = require('http-server')
+      const puppeteer = require('puppeteer')
+      const fs = require('fs')
+      const beautify = require('js-beautify').html
+      const server = createServer()
+      server.listen(8000)
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
+      await page.goto(`http://localhost:8000/demo/result.html`)
+      await page.screenshot({ path: `demo/screenshot.png`, fullPage: true })
+      const content = await page.content()
+      fs.writeFileSync(`demo/screenshot-src.html`, beautify(content))
+      server.close()
+      browser.close()
+    }
   ],
   lint: {
     ts: `tslint "src/**/*.ts" "html/**/*.ts"`,
@@ -26,24 +44,6 @@ module.exports = {
   test: [
     'tsc -p spec',
     'jasmine',
-    `node dist/index.js src/*.ts -o demo/result.html --exclude src/*.d.ts`,
-    async () => {
-      const { createServer } = require('http-server')
-      const puppeteer = require('puppeteer')
-      const fs = require('fs')
-      const beautify = require('js-beautify').html
-      const server = createServer()
-      server.listen(8000)
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
-      await page.goto(`http://localhost:8000/demo/result.html`)
-      await page.screenshot({ path: `demo/screenshot.png`, fullPage: true })
-      const content = await page.content()
-      fs.writeFileSync(`demo/screenshot-src.html`, beautify(content))
-      server.close()
-      browser.close()
-    },
     'git checkout demo/screenshot.png',
     async () => {
       const { stdout } = await execAsync('git status -s')
