610a27a2bed04f508d82fe1ea11d1cd825e95bca
york yao <yaoao12306@outlook.com>
2017-10-10 07:57:02 +0800

    remove screenshot source; change screenshot script to ts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,5 +1,6 @@
 const childProcess = require('child_process')
 const util = require('util')
+const { Service } = require('clean-scripts')
 
 const execAsync = util.promisify(childProcess.exec)
 
@@ -17,24 +18,7 @@ module.exports = {
         'cleancss -o html/vendor.bundle.css ./node_modules/tree-component/tree.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css ./node_modules/highlight.js/styles/routeros.css'
       ]
     },
-    `node dist/index.js src/*.ts -o demo/result.html --exclude src/*.d.ts`,
-    async () => {
-      const { createServer } = require('http-server')
-      const puppeteer = require('puppeteer')
-      const fs = require('fs')
-      const beautify = require('js-beautify').html
-      const server = createServer()
-      server.listen(8000)
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
-      await page.goto(`http://localhost:8000/demo/result.html`)
-      await page.screenshot({ path: `demo/screenshot.png`, fullPage: true })
-      const content = await page.content()
-      fs.writeFileSync(`demo/screenshot-src.html`, beautify(content))
-      server.close()
-      browser.close()
-    }
+    `node dist/index.js src/*.ts -o demo/result.html --exclude src/*.d.ts`
   ],
   lint: {
     ts: `tslint "src/**/*.ts" "html/**/*.ts"`,
@@ -44,7 +28,6 @@ module.exports = {
   test: [
     'tsc -p spec',
     'jasmine',
-    'git checkout demo/screenshot.png',
     async () => {
       const { stdout } = await execAsync('git status -s')
       if (stdout) {
@@ -57,5 +40,10 @@ module.exports = {
     ts: `tslint --fix "src/**/*.ts"`,
     js: `standard --fix "**/*.config.js"`
   },
-  release: `clean-release`
+  release: `clean-release`,
+  screenshot: [
+    new Service(`http-server -p 8000`),
+    `tsc -p screenshots`,
+    `node screenshots/index.js`
+  ]
 }
